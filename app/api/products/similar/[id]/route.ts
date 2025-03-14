7c97981f8ee6f7ef831dd7139f3e3244edb624f5
bhuter <btrjoseph77@gmail.com>
2025-03-14 21:22:35 +0200

    Similars

--- app/api/products/similar/[id]/route.ts
+++ app/api/products/similar/[id]/route.ts
@@ -17,7 +17,7 @@ export async function GET(req: Request, { params }: { params: { id: number } }) 
 
         // 1. Search for similar products by category
         result = await client.query(
-            `SELECT id, name, price, category, image 
+            `SELECT id, name, price, category, image, hashed_id 
              FROM products 
              WHERE category ILIKE '%' || $1 || '%' 
              AND id != $2`, [category, id]
@@ -27,7 +27,7 @@ export async function GET(req: Request, { params }: { params: { id: number } }) 
         if (result.rows.length <= 0) {
             const priceRange = 0.2; // Define price range e.g., 20% range
             result = await client.query(
-                `SELECT id, name, price, category, image 
+                `SELECT id, name, price, category, image, hashed_id 
                  FROM products 
                  WHERE price BETWEEN $1 AND $2
                  AND id != $3 ORDER BY price ASC`, 
@@ -38,7 +38,7 @@ export async function GET(req: Request, { params }: { params: { id: number } }) 
         // 3. If no products are found by price, search by name similarity
         if (result.rows.length <= 0) {
             result = await client.query(
-                `SELECT id, name, price, category, image 
+                `SELECT id, name, price, category, image, hashed_id 
                  FROM products 
                  WHERE name ILIKE '%' || $1 || '%'
                  AND id != $2`, [name, id]
